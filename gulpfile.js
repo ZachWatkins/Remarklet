73736fe8285be201c88e2d39225ab8e0c11adff6
ZachWatkins <watkinza@gmail.com>
2015-03-14 19:37:39 -0500

    Minifying the CSS file
    Things like replacing "normal" for "400" font-weights.

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,15 @@ var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
 
 gulp.task('default', function(){
-  return gulp.src('development/*.js')
+  gulp.src('development/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
     .pipe(uglify())
+    .pipe(gulp.dest('production'));
+  gulp.src('development/*.css')
+    .pipe(cssmin())
     .pipe(gulp.dest('production'));
 });
