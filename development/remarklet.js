cc7ea3b6b5559b051823ad7242b8d71eb73d8778
Zach Watkins <watkinza@gmail.com>
2015-03-29 22:47:07 -0500

    Removed Google Analytics
    Back when I started the project, it wasn't MIT licensed, and I wanted to
    know if anyone was finding it and using it on their own. Now it is MIT
    licensed and on Github, and I no longer feel comfortable loading GA with
    the script. I'm also planning to re-implement emmet's old textarea
    plug-in to add more functionality to the CSS editor and the Insert HTML
    prompt.

--- development/remarklet.js
+++ development/remarklet.js
@@ -638,7 +638,4 @@ require(['jquery', 'jqueryui', 'rangyinputs', 'stylesheet', 'storedobject', 'dup
 		controllers.bodyElements.toggle('on');
 	};
 	remarklet.init();
-	/* http://remysharp.com/2009/02/27/analytics-for-bookmarklets-injected-scripts/ */
-	function gaTrack(g,h,i){function c(e,j){return e+Math.floor(Math.random()*(j-e));}var f=1000000000,k=c(f,9999999999),a=c(10000000,99999999),l=c(f,2147483647),b=(new Date()).getTime(),d=window.location,m=new Image(),n='//www.google-analytics.com/__utm.gif?utmwv=1.3&utmn='+k+'&utmsr=-&utmsc=-&utmul=-&utmje=0&utmfl=-&utmdt=-&utmhn='+h+'&utmr='+d+'&utmp='+i+'&utmac='+g+'&utmcc=__utma%3D'+a+'.'+l+'.'+b+'.'+b+'.'+b+'.2%3B%2B__utmb%3D'+a+'%3B%2B__utmc%3D'+a+'%3B%2B__utmz%3D'+a+'.'+b+'.2.2.utmccn%3D(referral)%7Cutmcsr%3D'+d.host+'%7Cutmcct%3D'+d.pathname+'%7Cutmcmd%3Dreferral%3B%2B__utmv%3D'+a+'.-%3B';m.src=n;}
-	gaTrack('UA-44858109-1', 'remarklet.com', '/files/remarklet.js');
 });
