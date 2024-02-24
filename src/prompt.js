360553dce4795e601f0f876b7441cf5564d3f050
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-02-24 15:10:17 -0600

    Apply Prettier code style rules

--- src/prompt.js
+++ src/prompt.js
@@ -9,73 +9,79 @@
  * @link        https://github.com/zachwatkins/remarklet
  * @license     https://spdx.org/licenses/MIT.html MIT License
  */
-define(['jquery'], function($){
-	var callback, formobj;
-	var open = function(args){
-		if(typeof args.form == 'string'){
-			ui.form.html(args.form);
-		} else {
-			formobj = args.form;
-			ui.form.append(args.form);
-		}
-		args.init();
-		ui.window.show();
-		ui.content.css({'margin-top':function(){
-			return -1 * (ui.content.innerHeight()/2);
-		}});
-		ui.form.find('input,textarea').first().focus();
-		callback = args.callback;
-	};
-	var submit = function(){
-		var data = {};
-		ui.form.find('*[name]').each(function(){
-			data[this.name] = this.value;
-		});
-		callback(data);
-		close();
-	};
-	var close = function(){
-		ui.window.focus().blur().hide();
-		if(formobj){
-			formobj.remove();
-		}
-		ui.form.html('');
-	};
-	var ui = {
-		window: $('<div></div>').on('keydown', keydown),
-		form: $('<div></div>'),
-		content: $('<div></div>'),
-		submit: $('<button type="button">Submit</button>').on('click', submit),
-		cancel: $('<button type="button">Cancel</button>').on('click', close)
-	};
-	var keydown = function(e){
-		e.stopPropagation();
-		if(e.keyCode == 27){
-			/* Escape => Cancel form */
-			e.preventDefault();
-			close();
-		}
-	};
-	return {
-		init: function(prefix){
-			var key;
-			for(key in ui){
-				ui[key].attr('id', prefix+'-prompt-'+key);
-			}
-			ui.content.append(ui.form).append(ui.submit).append(ui.cancel).appendTo(ui.window);
-			ui.window.appendTo('body');
-		},
-		get: {
-			window: function(){
-				return ui.window;
-			},
-			form: function(){
-				return ui.form;
-			},
-			submit: function(){
-				return ui.submit;
-			}
-		},
-		open: open
-	};
+define(['jquery'], function ($) {
+    var callback, formobj;
+    var open = function (args) {
+        if (typeof args.form == 'string') {
+            ui.form.html(args.form);
+        } else {
+            formobj = args.form;
+            ui.form.append(args.form);
+        }
+        args.init();
+        ui.window.show();
+        ui.content.css({
+            'margin-top': function () {
+                return -1 * (ui.content.innerHeight() / 2);
+            },
+        });
+        ui.form.find('input,textarea').first().focus();
+        callback = args.callback;
+    };
+    var submit = function () {
+        var data = {};
+        ui.form.find('*[name]').each(function () {
+            data[this.name] = this.value;
+        });
+        callback(data);
+        close();
+    };
+    var close = function () {
+        ui.window.focus().blur().hide();
+        if (formobj) {
+            formobj.remove();
+        }
+        ui.form.html('');
+    };
+    var ui = {
+        window: $('<div></div>').on('keydown', keydown),
+        form: $('<div></div>'),
+        content: $('<div></div>'),
+        submit: $('<button type="button">Submit</button>').on('click', submit),
+        cancel: $('<button type="button">Cancel</button>').on('click', close),
+    };
+    var keydown = function (e) {
+        e.stopPropagation();
+        if (e.keyCode == 27) {
+            /* Escape => Cancel form */
+            e.preventDefault();
+            close();
+        }
+    };
+    return {
+        init: function (prefix) {
+            var key;
+            for (key in ui) {
+                ui[key].attr('id', prefix + '-prompt-' + key);
+            }
+            ui.content
+                .append(ui.form)
+                .append(ui.submit)
+                .append(ui.cancel)
+                .appendTo(ui.window);
+            ui.window.appendTo('body');
+        },
+        get: {
+            window: function () {
+                return ui.window;
+            },
+            form: function () {
+                return ui.form;
+            },
+            submit: function () {
+                return ui.submit;
+            },
+        },
+        open: open,
+    };
 });
--- src/storedobject.js
+++ src/storedobject.js
@@ -1,6 +1,6 @@
 /**
  * Remarklet - Stored Object module using RequireJS pattern.
- * 
+ *
  * @package     ZachWatkins\Remarklet
  * @subpackage  storedobject
  * @description Store object within module and localStorage, sync changes to both.
@@ -9,77 +9,77 @@
  * @link        https://github.com/zachwatkins/remarklet
  * @license     https://spdx.org/licenses/MIT.html MIT License
  */
-define(function(){
-	var dbname, // Name of database in localStorage.
-		defaults, // Default values of "dataset".
-		dataset = {},
-		topics = {},
-		hOP = topics.hasOwnProperty;
-	var set = function(propname, data){
-		// Send variable "data" to property "propname" of "dataset",
-		// or replace value of "dataset" with "propname" because "data" is undefined.
-		if(data){
-			dataset[propname] = data;
-		} else {
-			dataset = propname;
-		}
-		// Store JSON representation of "dataset" in localStorage.
-		localStorage[dbname] = JSON.stringify(dataset);
-		return this;
-	};
-	return {
-		init: function(uniquename, defaultvalues){
-			var name, storage;
-			dbname = uniquename;
-			dataset = defaults = defaultvalues;
-			// If localStorage has named data, it should extend "defaultvalues" (opinion)
-			// and merge with "dataset".
-			// If not, fill localStorage with defaultvalues.
-			if(localStorage[uniquename] !== undefined){
-				storage = JSON.parse(localStorage[uniquename]);
-				for(name in storage){
-					dataset[name] = storage[name];
-				}
-			} else {
-				localStorage[uniquename] = JSON.stringify(defaultvalues);
-			}
-			return this;
-		},
-		get: function(propname){
-			if(propname){
-				return dataset[propname];
-			} else {
-				return dataset;
-			}
-		},
-		set: set,
-		reset: function(){
-			set(defaults);
-			localStorage.removeItem(dbname);
-			return this;
-		},
-		subscribe: function(topic, listener) {
-			// Create the topic's object if not yet created
-			if(!hOP.call(topics, topic)) topics[topic] = [];
+define(function () {
+    var dbname, // Name of database in localStorage.
+        defaults, // Default values of "dataset".
+        dataset = {},
+        topics = {},
+        hOP = topics.hasOwnProperty;
+    var set = function (propname, data) {
+        // Send variable "data" to property "propname" of "dataset",
+        // or replace value of "dataset" with "propname" because "data" is undefined.
+        if (data) {
+            dataset[propname] = data;
+        } else {
+            dataset = propname;
+        }
+        // Store JSON representation of "dataset" in localStorage.
+        localStorage[dbname] = JSON.stringify(dataset);
+        return this;
+    };
+    return {
+        init: function (uniquename, defaultvalues) {
+            var name, storage;
+            dbname = uniquename;
+            dataset = defaults = defaultvalues;
+            // If localStorage has named data, it should extend "defaultvalues" (opinion)
+            // and merge with "dataset".
+            // If not, fill localStorage with defaultvalues.
+            if (localStorage[uniquename] !== undefined) {
+                storage = JSON.parse(localStorage[uniquename]);
+                for (name in storage) {
+                    dataset[name] = storage[name];
+                }
+            } else {
+                localStorage[uniquename] = JSON.stringify(defaultvalues);
+            }
+            return this;
+        },
+        get: function (propname) {
+            if (propname) {
+                return dataset[propname];
+            } else {
+                return dataset;
+            }
+        },
+        set: set,
+        reset: function () {
+            set(defaults);
+            localStorage.removeItem(dbname);
+            return this;
+        },
+        subscribe: function (topic, listener) {
+            // Create the topic's object if not yet created
+            if (!hOP.call(topics, topic)) topics[topic] = [];
 
-			// Add the listener to queue
-			var index = topics[topic].push(listener) -1;
+            // Add the listener to queue
+            var index = topics[topic].push(listener) - 1;
 
-			// Provide handle back for removal of topic
-			return {
-				remove: function() {
-					delete topics[topic][index];
-				}
-			};
-		},
-		publish: function(topic, info) {
-			// If the topic doesn't exist, or there's no listeners in queue, just leave
-			if(!hOP.call(topics, topic)) return;
+            // Provide handle back for removal of topic
+            return {
+                remove: function () {
+                    delete topics[topic][index];
+                },
+            };
+        },
+        publish: function (topic, info) {
+            // If the topic doesn't exist, or there's no listeners in queue, just leave
+            if (!hOP.call(topics, topic)) return;
 
-			// Cycle through topics queue, fire!
-			topics[topic].forEach(function(item) {
-				item(info !== undefined ? info : {});
-			});
-		}
-	};
+            // Cycle through topics queue, fire!
+            topics[topic].forEach(function (item) {
+                item(info !== undefined ? info : {});
+            });
+        },
+    };
 });
