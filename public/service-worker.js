6ad39a4ac091df1a90586e5a2c91d22815b42c42
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-03-11 22:38:47 -0500

    Remove unneeded features

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,37 +1,5 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.action.setBadgeText({
-        text: "OFF",
-    });
-});
-
-chrome.action.onClicked.addListener(async (tab) => {
-    // Retrieve the action badge to check if the extension is 'ON' or 'OFF'.
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-    // Next state will always be the opposite.
-    const nextState = prevState === 'ON' ? 'OFF' : 'ON'
-
-    // Set the action badge to the next state.
-    await chrome.action.setBadgeText({
-        tabId: tab.id,
-        text: nextState,
-    });
-
-    if (nextState === "ON") {
-      // Insert the CSS file when the user turns the extension on.
-      await chrome.scripting.insertCSS({
-        files: ["style.css"],
-        target: { tabId: tab.id },
-      });
-      // Execute the script when the user turns the extension on.
-      await chrome.scripting.executeScript({
-        target : { tabId : tab.id },
-        files : [ "remarklet.iife.js" ],
-      });
-    } else if (nextState === "OFF") {
-      // Remove the CSS file when the user turns the extension off.
-      await chrome.scripting.removeCSS({
-        files: ["style.css"],
-        target: { tabId: tab.id },
-      });
-    }
+chrome.runtime.onInstalled.addListener(({ reason }) => {
+  if (reason === 'install') {
+      chrome.storage.local.set({ ON: true });
+  }
 });
