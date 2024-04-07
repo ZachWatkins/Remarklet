5e7bd9f9bc0fe9da203b207b558391fd890b9e16
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2024-04-07 16:16:51 -0500

    Add new files

---
+++ packages/chrome-extension/popup.js
@@ -0,0 +1,73 @@
+const init = async function () {
+    const toggleBtnOn = document.querySelector('#btn_on');
+    const toggleBtnOff = document.querySelector('#btn_off');
+    toggleBtnOff.classList.add('on');
+
+    toggleBtnOn.addEventListener('click', async () => {
+        const [tab] = await chrome.tabs.query({
+            active: true,
+            lastFocusedWindow: true,
+        });
+        if (!tab.url) {
+            return;
+        }
+        const host = new URL(tab.url).host;
+        chrome.runtime
+            .sendMessage({
+                type: 'setExtensionStatus',
+                host,
+                status: true,
+            })
+            .then(() => {
+                toggleBtnOn.classList.add('on');
+                toggleBtnOff.classList.remove('on');
+            });
+    });
+
+    toggleBtnOff.addEventListener('click', async () => {
+        const [tab] = await chrome.tabs.query({
+            active: true,
+            lastFocusedWindow: true,
+        });
+        if (!tab.url) {
+            return;
+        }
+        const host = new URL(tab.url).host;
+        chrome.runtime
+            .sendMessage({
+                type: 'setExtensionStatus',
+                host,
+                status: false,
+            })
+            .then(() => {
+                toggleBtnOn.classList.remove('on');
+                toggleBtnOff.classList.add('on');
+            });
+    });
+
+    const [tab] = await chrome.tabs.query({
+        active: true,
+        lastFocusedWindow: true,
+    });
+
+    if (!tab.url) {
+        return;
+    }
+
+    const message = {
+        type: 'getExtensionStatus',
+        host: new URL(tab.url).host,
+    };
+
+    chrome.runtime.sendMessage(message).then((response) => {
+        if (response.status) {
+            toggleBtnOn.classList.add('on');
+            toggleBtnOff.classList.remove('on');
+        } else {
+            toggleBtnOn.classList.remove('on');
+            toggleBtnOff.classList.add('on');
+        }
+    });
+};
+
+document.addEventListener('DOMContentLoaded', init);
