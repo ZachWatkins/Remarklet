94e2b5ca67a3bb7817f101465f2d1908d9bfe6aa
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-03-06 18:58:53 -0600

    Remove variable reference

--- packages/chrome-extension-remake/lib/index.js
+++ packages/chrome-extension-remake/lib/index.js
@@ -4,7 +4,7 @@ chrome.runtime.onMessage.addListener(function (message) {
     console.log('Content script received message:', message);
     if (message.type === 'setExtensionStatus') {
         if (message.value) {
-            console.log('Activating extension', state);
+            console.log('Activating extension');
             setMode('dragging');
         } else {
             console.log('Deactivating extension');
