b9a4a06a32c198d624381f360a476511ce58bcbc
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-03-09 13:19:29 -0500

    Add textedit module

--- packages/chrome-extension-remake/lib/index.js
+++ packages/chrome-extension-remake/lib/index.js
@@ -2,9 +2,11 @@ import store from "./store.js";
 import drag from "./drag.js";
 import target from "./target.js";
 import styles from "./styles.js";
+import textedit from "./textedit.js";
 styles();
 drag();
 target();
+textedit();
 
 chrome.runtime.onMessage.addListener(function (message) {
     console.log('Content script received message:', message);
