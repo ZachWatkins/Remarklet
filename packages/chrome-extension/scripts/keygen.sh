fabdb0e51aa01fcf52511ba54d8f84d18ada258f
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-03-09 18:51:14 -0500

    Remove Chrome package folders

--- packages/chrome-extension/scripts/keygen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-openssl genpkey -algorithm RSA -out private.pem -pkeyopt rsa_keygen_bits:2048
-openssl rsa -pubout -in private.pem -out public.pem
-openssl rsa -pubin -inform PEM -outform DER -in public.pem | base64 > key.txt
