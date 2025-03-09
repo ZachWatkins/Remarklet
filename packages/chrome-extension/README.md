7900325adf1cf6051f978054a7171bc2689dad33
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-03-09 18:51:14 -0500

    Remove Chrome package folders

--- packages/chrome-extension/README.md
+++
@@ -1,3 +0,0 @@
-# Remarklet Chrome Extension
-
-This is a Chrome extension for people to modify a web page using their mouse and keyboard.
--- public/service-worker.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onInstalled.addListener(({ reason }) => {
-  if (reason === 'install') {
-      chrome.storage.local.set({ ON: true });
-  }
-});
