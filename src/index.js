047f897da0a714e8639c2a58769599901b6785b8
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-03-09 16:53:15 -0500

    Update root library file to work as a module

--- src/index.js
+++ src/index.js
@@ -8,18 +8,9 @@ drag();
 target();
 textedit();
 
-chrome.runtime.onMessage.addListener(function (message) {
-    if (message.type === 'remarkletSetExtensionStatus') {
-        if (message.value) {
-            store.set('active', true);
-        } else {
-            store.set('active', false);
-        }
-    }
-});
-
-chrome.storage.sync.get("status", (result) => {
-    if (result.status === true) {
-        store.set('active', true);
-    }
-});
+export function activate() {
+    store.set('active', true);
+}
+export function deactivate() {
+    store.set('active', false);
+}
