e6d815e9a5efaa6e80acec7b6e6d56f62fe83436
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-03-09 21:36:56 -0500

    Declare port for test app

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: 'http://localhost:4173',
+        baseURL: 'http://localhost:3000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
@@ -73,7 +73,7 @@ export default defineConfig({
     webServer: {
         cwd: 'test',
         command: 'npm run serve',
-        url: 'http://localhost:4173',
+        url: 'http://localhost:3000',
         reuseExistingServer: !process.env.CI,
     },
 });
