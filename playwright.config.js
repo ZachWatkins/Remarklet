c4c3827795a634d75b85d3d7891881821f4206bf
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-03-09 21:18:06 -0500

    Update test server config

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: 'http://localhost:5173',
+        baseURL: 'http://localhost:4173',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: 'on-first-retry',
@@ -71,8 +71,9 @@ export default defineConfig({
 
     /* Run your local dev server before starting the tests */
     webServer: {
-        command: 'npm run start',
-        url: 'http://localhost:5173',
+        cwd: 'test',
+        command: 'npm run serve',
+        url: 'http://localhost:4173',
         reuseExistingServer: !process.env.CI,
     },
 });
