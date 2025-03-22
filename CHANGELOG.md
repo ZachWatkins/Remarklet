b4162167e299189681e38e52d329fefa8109d882
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2025-03-21 21:04:54 -0500

    Add stylesheet module, avoid highlighting text while resizing and dragging (#47)
    * Add stylesheet module refactored from original module
    
    * Move the stylesheet module and correct how its text is handled
    
    * Implement the new Stylesheet module
    
    * Update changelog
    
    * Apply lint fixes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,11 +11,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 - Added a console warning when users try to resize a rotated element: "Remarklet does not yet support resizing rotated elements."
 - Added status badges to the README.md file.
 - Added a contributor section to the README.md file.
+- Added a stylesheet utility module to handle creating a stylesheet for the library that can receive updates based on state changes.
 
 ### Changed
 
 - Resizing will now only set the width or height of the element during the action, not both at the same time.
-- Fixed visual issue where resizing elements caused the text inside of the element to be selected.
+- Fixed visual issue where resizing elements caused the text inside of elements to be selected.
 - Fixed issue where dragging elements did not disable the content editing mode.
 - Fixed issue where elements with `box-sizing: content-box` and padding were not resized correctly.
 
---
+++ src/utils/stylesheet.js
@@ -0,0 +1,49 @@
+/**
+ * Remarklet - Stylesheet Module.
+ *
+ * @package     ZachWatkins\Remarklet
+ * @subpackage  Stylesheet
+ * @description Module for creating and manipulating a stylesheet programmatically.
+ * @author      Zachary Kendall Watkins <zwatkins.it@gmail.com> (https://github.com/zachwatkins)
+ * @copyright   Zachary Kendall Watkins Copyright (c) 2014-2025 All Rights Reserved.
+ * @link        https://github.com/zachwatkins/remarklet
+ * @license     https://spdx.org/licenses/MIT.html MIT License
+ */
+export default function Stylesheet(value) {
+    this.element = document.createElement("style");
+    this.element.innerText = value || "";
+    var indent = "    ";
+    var rules = {};
+    this.setRule = (selector, rule) => {
+        if (!rule) return;
+        var ruletext,
+            found = false,
+            i = this.element.sheet.cssRules.length - 1;
+        ruletext = "{\n" + indent + rule.replace(/; (\w)/g, ";\n" + indent + "$1") + "\n}";
+        while (i >= 0) {
+            if (selector == this.element.sheet.cssRules[i].selectorText) {
+                found = this.element.sheet.cssRules[i];
+                i = 0;
+            }
+            i--;
+        }
+        if (!found) {
+            i = this.element.sheet.cssRules.length;
+        }
+        this.element.sheet.insertRule(selector + ruletext, i);
+        rules[selector] = ruletext;
+    };
+    this.setString = (str) => {
+        if (this.element.textContent === str) {
+            return;
+        }
+        this.element.sheet.replaceSync(str);
+        rules = {};
+        for (let i = 0; i < this.element.sheet.cssRules.length; i++) {
+            let rule = this.element.sheet.cssRules[i];
+            let selector = rule.cssText.split("{")[0].trim();
+            let ruleText = rule.cssText.split("{", 2)[1].trim();
+            rules[selector] = ruleText;
+        }
+    };
+}
