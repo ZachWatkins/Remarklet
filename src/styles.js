902dd2770b422961784cda298d829781aa0d0b7c
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-03-21 20:52:02 -0500

    Implement the new Stylesheet module

--- src/styles.js
+++ src/styles.js
@@ -2,18 +2,34 @@
  * @module styles
  * Add a stylesheet to the document containing the library's styles.
  */
+import store from "./store.js";
+import Stylesheet from "./utils/stylesheet.js";
+
 export default function main() {
-    const styles = `
+    const stylesheet = new Stylesheet(`
 [data-remarklet-highlight] {
     outline: 2px solid #00b3dd;
 }
-[data-remarklet-dragging],
-[data-remarklet-resizing] {
+body[data-remarklet-dragging],
+body[data-remarklet-resizing] {
     user-select: none !important;
     -webkit-user-select: none !important;
 }
-`;
-    const styleSheet = document.createElement("style");
-    styleSheet.innerText = styles;
-    document.head.appendChild(styleSheet);
+`);
+    document.head.appendChild(stylesheet.element);
+
+    store.subscribe("mode", (mode, lastMode) => {
+        // When the mode is resizing or dragging, add a style to the body using the stylesheet that gives it "user-select: none".
+        if ("dragging" === mode) {
+            document.body.setAttribute("data-remarklet-dragging", "true");
+            if ("resizing" === lastMode) {
+                document.body.removeAttribute("data-remarklet-resizing");
+            }
+        } else if ("resizing" === mode) {
+            document.body.setAttribute("data-remarklet-resizing", "true");
+            if ("dragging" === lastMode) {
+                document.body.removeAttribute("data-remarklet-dragging");
+            }
+        }
+    });
 }
