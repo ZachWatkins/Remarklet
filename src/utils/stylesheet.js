9e11c53e625e85e42ed987517d461b707bc37ff7
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-03-21 20:51:40 -0500

    Move the stylesheet module and correct how its text is handled

--- src/utils/stylesheet.js
+++ src/utils/stylesheet.js
@@ -11,46 +11,43 @@
  */
 export default function Stylesheet(value) {
     this.element = document.createElement('style');
+    this.element.innerText = value || '';
     var indent = '    ';
     var rules = {};
-    this.setRule = function (selector, rule) {
+    this.setRule = (selector, rule) => {
         if (!rule) return;
         var ruletext,
             found = false,
-            i = element.sheet.cssRules.length - 1;
+            i = this.element.sheet.cssRules.length - 1;
         ruletext =
             '{\n' +
             indent +
             rule.replace(/; (\w)/g, ';\n' + indent + '$1') +
             '\n}';
         while (i >= 0) {
-            if (selector == element.sheet.cssRules[i].selectorText) {
-                found = element.sheet.cssRules[i];
+            if (selector == this.element.sheet.cssRules[i].selectorText) {
+                found = this.element.sheet.cssRules[i];
                 i = 0;
             }
             i--;
         }
         if (!found) {
-            i = element.sheet.cssRules.length;
+            i = this.element.sheet.cssRules.length;
         }
-        element.sheet.insertRule(selector + ruletext, i);
+        this.element.sheet.insertRule(selector + ruletext, i);
         rules[selector] = ruletext;
     };
-    this.setString = function (str) {
-        if (element.textContent === str) {
+    this.setString = (str) => {
+        if (this.element.textContent === str) {
             return;
         }
-        element.textContent = str;
+        this.element.sheet.replaceSync(str);
         rules = {};
-        for (let i = 0; i < element.cssRules.length; i++) {
-            /** @type {CSSStyleRule} */
-            let rule = element.cssRules[i];
+        for (let i = 0; i < this.element.sheet.cssRules.length; i++) {
+            let rule = this.element.sheet.cssRules[i];
             let selector = rule.cssText.split('{')[0].trim();
             let ruleText = rule.cssText.split('{', 2)[1].trim();
             rules[selector] = ruleText;
         }
     };
-    if (value) {
-        this.setString(value);
-    }
 }
