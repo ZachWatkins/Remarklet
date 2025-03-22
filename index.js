34b666e2477389db4792b7ed74231efe00a2c383
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2025-03-21 21:31:22 -0500

    Document the public interface (#49)
    * Document the public interface
    
    * Apply formatting

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ let initialized = false;
  * console.log(remarklet.version);
  */
 function remarklet() {}
+
+/**
+ * Activates the Remarklet library, initializing all necessary components.
+ * @example
+ * remarklet.activate();
+ */
 remarklet.activate = function () {
     if (!initialized) {
         initialized = true;
@@ -36,9 +42,20 @@ remarklet.activate = function () {
     }
     store.set("active", true);
 };
+/**
+ * Deactivates the Remarklet library, cleaning up any resources or event listeners.
+ * @example
+ * remarklet.deactivate();
+ */
 remarklet.deactivate = function () {
     store.set("active", false);
 };
+/**
+ * Get the current version of the Remarklet library.
+ * @example
+ * remarklet.version;
+ * // "1.0.4"
+ */
 remarklet.version = pkg.version;
 
 export default remarklet;
