68f4b86d67eb33088bbac05971cc31791af056d3
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-04-13 13:51:26 -0500

    wip

--- src/changeMap.js
+++ src/changeMap.js
@@ -5,7 +5,7 @@ const elementChangeMap = new WeakMap();
 let store = null;
 
 /**
- * @typedef {Object} ChangeMapItem
+ * @typedef {Object} ElementState
  * @property {boolean} restored - Whether the item has been restored.
  * @property {string} initialStyle - The initial style of the item.
  * @property {Object} delta - The delta of the item.
@@ -22,6 +22,7 @@ let store = null;
  * @property {string} style.height - The height of the item.
  * @property {string} style.marginBottom - The margin bottom of the item.
  * @property {string} style.marginRight - The margin right of the item.
+ * @property {boolean} matrix3d - Whether the element position should be styled using matrix3d.
  * @property {() => string} getStyleRule - Returns the style rule for the item.
  * @property {(deltaX: number, deltaY: number) => void} move - Moves the item by deltaX and/or deltaY.
  */
@@ -30,118 +31,104 @@ let store = null;
  * @param {HTMLElement} target - The target element.
  * @param {Object} [props] - The properties of the change map item.
  * @param {boolean} [props.restored] - Whether the item has been restored.
- * @param {string} [props.initialStyle] - The initial style of the item.
- * @param {Object} [props.delta] - The delta of the item.
- * @param {number} [props.delta.x] - The x coordinate change.
- * @param {number} [props.delta.y] - The y coordinate change.
- * @param {number} [props.delta.width] - The width change.
- * @param {number} [props.delta.height] - The height change.
  * @param {boolean} [props.dragged] - Whether the item has been dragged.
  * @param {boolean} [props.resized] - Whether the item has been resized.
- * @param {string} [props.selector] - The selector of the item.
- * @param {Object} [props.style] - The style of the item.
- * @param {string} [props.style.transform] - The transform of the item.
- * @param {string} [props.style.width] - The width of the item.
- * @param {string} [props.style.height] - The height of the item.
- * @param {string} [props.style.marginBottom] - The margin bottom of the item.
- * @param {string} [props.style.marginRight] - The margin right of the item.
  */
-function ChangeMapItem(target, props = {}) {
-    this.restored = props.restored || false;
-    this.initialStyle =
-        props.initialStyle ||
-        target.style.cssText.replace(/cursor:[^;]+;?/g, "");
-    this.delta = props.delta || {
-        x: 0,
-        y: 0,
-        width: 0,
-        height: 0,
-    };
-    this.dragged = props.dragged || false;
-    this.resized = props.resized || false;
+function ElementState(target, props = {}) {
+    const computed = window.getComputedStyle(target);
     this.selector =
         props.selector ||
         getUniqueSelector(target, {
             excludeDataAttributePrefix: "remarklet",
         });
-    if (props.style) {
-        this.style = { ...props.style };
+    this.initialStyle = target.style.cssText.replace(/cursor:[^;]+;?/g, "");
+    this.restored = props.restored || false;
+    this.dragged = props.dragged || false;
+    this.resized = props.resized || false;
+    this.width = parseFloat(computed.width);
+    this.height = parseFloat(computed.height);
+    this.marginBottom = parseFloat(computed.marginBottom);
+    this.marginRight = parseFloat(computed.marginRight);
+    this.matrix3d = computed.transform.indexOf("matrix3d(") >= 0;
+    if (computed.transform === "none") {
+        this.transform = [1, 0, 0, 1, 0, 0];
+    } else if (!this.matrix3d) {
+        const matrixRegex = /matrix\(([^)]+)\)/;
+        const matrixMatch = computed.transform.match(matrixRegex);
+        if (matrixMatch) {
+            this.transform = matrixMatch[1]
+                .split(",")
+                .map((v) => parseFloat(v));
+        } else {
+            // This shouldn't happen, but just in case.
+            this.transform = [1, 0, 0, 1, 0, 0];
+        }
     } else {
-        this.style = {
-            transform: target.style.transform || "none",
-            width: target.style.width,
-            height: target.style.height,
-            cursor: target.style.cursor,
-        };
+        const matrix3dRegex = /matrix3d\(([^)]+)\)/;
+        const matrix3dMatch = computed.transform.match(matrix3dRegex);
+        if (matrix3dMatch) {
+            this.transform = matrix3dMatch[1]
+                .split(",")
+                .map((v) => parseFloat(v));
+        } else {
+            this.transform = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+        }
     }
-    // If the item is restored, we need to
-    this.getStyleRule = function () {
-        const styles = this.style;
-        let rule = [];
-        for (const key in styles) {
-            if (styles[key] === null) {
-                continue;
-            }
-            const kebabKey = key.replace(
-                /([A-Z])/g,
-                (match) => `-${match.toLowerCase()}`,
-            );
-            rule.push(`${kebabKey}: ${styles[key]}`);
-        }
-        if (rule.length === 0) {
-            return "";
-        }
-        return rule.join(";\n") + ";";
-    };
+    if (!this.matrix3d) {
+        Object.defineProperty(this, "x", {
+            get() {
+                return this.transform[4];
+            },
+            set(value) {
+                this.transform[4] = value;
+            },
+        });
+        Object.defineProperty(this, "y", {
+            get() {
+                return this.transform[5];
+            },
+            set(value) {
+                this.transform[5] = value;
+            },
+        });
+    } else {
+        Object.defineProperty(this, "x", {
+            get() {
+                return this.transform[12];
+            },
+            set(value) {
+                this.transform[12] = value;
+            },
+        });
+        Object.defineProperty(this, "y", {
+            get() {
+                return this.transform[13];
+            },
+            set(value) {
+                this.transform[13] = value;
+            },
+        });
+    }
+    Object.defineProperty(this, "rule", {
+        get() {
+            return `
+width: ${this.width}px;
+height: ${this.height}px;
+margin-bottom: ${this.marginBottom}px;
+margin-right: ${this.marginRight}px;
+transform: ${this.matrix3d ? "matrix3d" : "matrix"}(${this.transform.join(", ")});
+`;
+        },
+    });
     /**
-     * Move the item by deltaX and/or deltaY, updating the change item's
-     * style.transform, delta.x, and delta.y properties.
+     * Move the item by deltaX and deltaY.
      * @param {number} deltaX The x coordinate change.
      * @param {number} deltaY The y coordinate change.
      * @returns {void}
      */
     this.move = function (deltaX, deltaY) {
-        this.delta.x += deltaX;
-        this.delta.y += deltaY;
-        let style = "";
-        const originalTransform =
-            elementTransforms.get(target) || storeOriginalTransform(target);
-        if ("none" === this.style.transform) {
-            style = `matrix(1, 0, 0, 1, ${x}, ${y})`;
-        } else if (originalTransform.indexOf("matrix(") >= 0) {
-            const matrixRegex = /matrix\(([^)]+)\)/;
-            const matrixMatch = originalTransform.match(matrixRegex);
-            let vals = [1, 0, 0, 1, 0, 0];
-            if (matrixMatch) {
-                vals = matrixMatch[1].split(",").map((v) => parseFloat(v));
-            }
-            const a = parseFloat(vals[0]);
-            const b = parseFloat(vals[1]);
-            const c = parseFloat(vals[2]);
-            const d = parseFloat(vals[3]);
-            const e = parseFloat(vals[4]);
-            const f = parseFloat(vals[5]);
-            // Calculate the new translation values.
-            x = e + x;
-            y = f + y;
-            // Create the new matrix string.
-            style = `matrix(${a}, ${b}, ${c}, ${d}, ${x}, ${y})`;
-        } else {
-            const matrix3dRegex = /matrix3d\(([^)]+)\)/;
-            const matrix3dMatch = originalTransform.match(matrix3dRegex);
-            let vals = [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
-            if (matrix3dMatch) {
-                vals = matrix3dMatch[1].split(",").map((v) => parseFloat(v));
-            }
-            x = vals[12] = vals[0] * x + vals[4] * y + vals[12];
-            y = vals[13] = vals[1] * x + vals[5] * y + vals[13];
-            style = "matrix3d(" + vals.join(", ") + ")";
-        }
-        return {
-            x,
-            y,
-            style,
-        };
+        this.x = this.x + deltaX;
+        this.y = this.y + deltaY;
     };
 }
 
@@ -163,17 +150,6 @@ changeMap.get = function (target) {
     return elementChangeMap.get(target);
 };
 
-changeMap.set = function (target, value) {
-    elementChangeMap.set(target, value);
-    if (store) {
-        const selector = getUniqueSelector(target, {
-            excludeDataAttributePrefix: "remarklet",
-        });
-        store.value[selector] = value;
-        store.store();
-    }
-};
-
 changeMap.sync = function (target) {
     if (!store) {
         return;
@@ -189,24 +165,23 @@ changeMap.init = function (target, mode) {
     if (elementChangeMap.has(target)) {
         return;
     }
-    const selector = getUniqueSelector(target, {
-        excludeDataAttributePrefix: "remarklet",
-    });
     if (!store) {
         elementChangeMap.set(
             target,
-            new ChangeMapItem(target, {
-                selector,
+            new ElementState(target, {
                 [mode]: true,
             }),
         );
         return;
     }
+    const selector = getUniqueSelector(target, {
+        excludeDataAttributePrefix: "remarklet",
+    });
     const previousSession = store.value[selector];
     if (!previousSession) {
         elementChangeMap.set(
             target,
-            new ChangeMapItem(target, {
+            new ElementState(target, {
                 selector,
                 [mode]: true,
             }),
@@ -217,7 +192,7 @@ changeMap.init = function (target, mode) {
     }
     elementChangeMap.set(
         target,
-        new ChangeMapItem(target, {
+        new ElementState(target, {
             ...JSON.parse(JSON.stringify(previousSession)),
             restored: true,
             [mode]: true,
