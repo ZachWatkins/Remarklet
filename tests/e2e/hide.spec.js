64d358796f84ae6e661d8ded9996132d23234874
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-04-20 09:35:15 -0500

    Update test

--- tests/e2e/hide.spec.js
+++ tests/e2e/hide.spec.js
@@ -1,5 +1,5 @@
 // @ts-check
-// This file tests whether there are runtime errors simply from adding the script to the page.
+// This file tests whether an element can be hidden by being dragged to the top right corner of the webpage and then released.
 import { test, expect } from "@playwright/test";
 
 test("can hide elements", async ({ page }) => {
@@ -13,4 +13,29 @@ test("can hide elements", async ({ page }) => {
     expect(text).toHaveCount(1);
     expect(text).toBeVisible();
     await text.scrollIntoViewIfNeeded();
+    const boundingBox = await text.boundingBox();
+    if (!boundingBox) {
+        throw new Error("Bounding box is null");
+    }
+    const start = {
+        x: boundingBox.x + boundingBox.width / 2,
+        y: boundingBox.y + boundingBox.height / 2,
+    };
+    await page.mouse.move(start.x, start.y);
+    await page.mouse.down();
+    await page.mouse.move(start.x + 10, start.y - 10);
+    const hideZone = await page.getByText("Hide");
+    const hideZoneBox = await hideZone.boundingBox();
+    if (!hideZoneBox) {
+        throw new Error("Hide zone bounding box is null");
+    }
+    const end = {
+        x: hideZoneBox.x + hideZoneBox.width / 2,
+        y: hideZoneBox.y + hideZoneBox.height / 2,
+    };
+    await page.mouse.move(end.x, end.y);
+    await page.mouse.up();
+    await page.waitForTimeout(100);
+    const hiddenText = await page.getByText(textString);
+    expect(hiddenText).toHaveCount(0);
 });
