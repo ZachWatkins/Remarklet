609475d32c73281ff1d638f8fb17bd0a69fdc6d8
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-04-20 09:50:35 -0500

    Improve test

--- tests/e2e/hide.spec.js
+++ tests/e2e/hide.spec.js
@@ -37,5 +37,6 @@ test("can hide elements", async ({ page }) => {
     await page.mouse.up();
     await page.waitForTimeout(100);
     const hiddenText = await page.getByText(textString);
-    expect(hiddenText).toHaveCount(0);
+    expect(hiddenText).toHaveCount(1);
+    expect(hiddenText).not.toBeVisible();
 });
