d676611c0e2a2b64041de003b01fceb4594ee94d
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-04-24 21:34:57 -0500

    Fix test

--- tests/e2e/hide.spec.js
+++ tests/e2e/hide.spec.js
@@ -3,7 +3,7 @@
 import { test, expect } from "@playwright/test";
 
 test("can hide elements", async ({ page }) => {
-    await page.goto("/");
+    await page.goto("?hide=true");
     const textString = "A demonstration of what can be accomplished";
     const text = await page.getByText(textString);
     expect(text).toHaveCount(1);
