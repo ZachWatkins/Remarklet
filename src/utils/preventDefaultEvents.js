a999409e1a87f1b5cc9a07809a164a15bb9e7b4f
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-04-27 13:40:49 -0500

    function to instance to avoid removing other instances' blocks

--- src/utils/preventDefaultEvents.js
+++ src/utils/preventDefaultEvents.js
@@ -1,12 +1,12 @@
-function preventDefaultEvent(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    event.stopImmediatePropagation();
-}
-
 export default function preventDefaultEvents(events) {
     this.preventing = {};
     this.events = [...events];
+
+    this.preventDefaultEvent = function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+    };
 
     /**
      * Prevents default events for the specified actions.
@@ -18,7 +18,7 @@ export default function preventDefaultEvents(events) {
                 this.preventing[this.events[i]] = true;
                 document.body.addEventListener(
                     this.events[i],
-                    preventDefaultEvent,
+                    this.preventDefaultEvent,
                     {
                         capture: true,
                     },
@@ -37,7 +37,7 @@ export default function preventDefaultEvents(events) {
                 this.preventing[this.events[i]] = false;
                 document.body.removeEventListener(
                     this.events[i],
-                    preventDefaultEvent,
+                    this.preventDefaultEvent,
                     {
                         capture: true,
                     },
