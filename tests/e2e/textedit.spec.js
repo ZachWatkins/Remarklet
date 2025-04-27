330b847738cbe4dfedc6aaf71a3feec8b674fa05
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-04-27 13:55:41 -0500

    Add test for text editing feature

---
+++ tests/e2e/textedit.spec.js
@@ -0,0 +1,33 @@
+// @ts-check
+// This file tests whether there are runtime errors simply from adding the script to the page.
+import { test, expect } from "@playwright/test";
+
+test("can edit element text that has a mouse event without triggering that event", async ({
+    page,
+}) => {
+    await page.goto("/example-app.html");
+    page.on("dialog", (dialog) => {
+        test.fail(
+            true,
+            "The dialog should not be triggered if the button click event is being suppressed as expected.",
+        );
+        dialog.accept();
+    });
+    const text = "Button with event handler on click.";
+    const button = await page.getByText(text);
+    await button.scrollIntoViewIfNeeded();
+    const boundingBox = await button.boundingBox();
+    if (!boundingBox) {
+        throw new Error("Bounding box is null");
+    }
+    const start = {
+        x: boundingBox.x + boundingBox.width / 2,
+        y: boundingBox.y + boundingBox.height / 2,
+    };
+    await page.mouse.click(start.x, start.y);
+    await page.keyboard.press("End");
+    await page.keyboard.type("New text to be added");
+    await page.waitForTimeout(100);
+    const updatedText = await page.getByText("New text to be added");
+    expect(updatedText).toBeTruthy();
+});
