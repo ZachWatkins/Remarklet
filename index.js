ba0186de7d587707e75447b99edc1f61282b3063
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-05-02 21:05:17 -0500

    Sort by version introduced

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /**
  * @module @zw/remarklet
  * @description Remarklet is a library for visually manipulating web page content.
+ * @author Zachary Kendall Watkins
  * @copyright 2014-present Zachary Kendall Watkins. All rights reserved.
  * @license
  * Licensed under the MIT License. See LICENSE.txt in the project root or go to
@@ -59,88 +60,15 @@ const app = {
 
 /**
  * @alias module:@zw/remarklet
- * @type {function}
+ * @function
  * @returns {void}
  */
 function remarklet() {}
 
 /**
- * @alias module:@zw/remarklet.version
- * @since 1.0.2
- * @type {string}
- * @description Get the current version of the Remarklet library.
- */
-remarklet.version = pkg.version;
-
-/**
- * @alias module:@zw/remarklet.config
- * @since 1.3.0
- * @type {function}
- * @description Configures the library's features.
- * @param {Object} options - The configuration options.
- * @param {boolean} options.persist - Whether to persist the state of the page between sessions.
- * @param {boolean} options.hide - Whether to hide certain elements.
- * @returns {void}
- */
-remarklet.config = function (options) {
-    if (options.persist === true) {
-        config.persist = true;
-    }
-    if (options.hide === true) {
-        config.hide = true;
-    }
-};
-
-/**
- * @deprecated
- * @alias module:@zw/remarklet.options
- * @since 1.1.0
- * @type {function}
- * @method
- * @description Deprecated. An alias of remarklet.config. Will be removed in v2.0.0.
- * @param {Object} options - The configuration options.
- * @param {boolean} options.persist - Whether to persist the state of the page between sessions.
- * @param {boolean} options.hide - Whether to hide certain elements.
- * @returns {void}
- */
-remarklet.options = remarklet.config;
-
-/**
- * @alias module:@zw/remarklet.restore
- * @since 1.2.0
- * @type {function}
- * @description Restores the persisted changes, if any. Runs before the interactive features are initialized.
- * @returns {void}
- */
-remarklet.restore = function () {
-    if (state.get("loading") === true) {
-        console.warn("Loading is already in progress.");
-        return;
-    }
-    state.set("loading", true);
-    if (config.persist !== true) {
-        config.persist = true;
-    }
-    app.use(changeMap);
-    app.use(styles);
-    state.set("loading", false);
-};
-
-/**
- * @alias module:@zw/remarklet.unstore
- * @since 1.3.0
- * @type {function}
- * @description Deletes all Remarklet data from localStorage.
- * @returns {void}
- */
-remarklet.unstore = function () {
-    changeMap.unstore();
-};
-
-/**
  * @alias module:@zw/remarklet.activate
  * @since 1.0.0
- * @type {function}
+ * @function
  * @description Activates the Remarklet library, initializing all necessary components.
  * @returns {void}
  */
@@ -164,12 +92,84 @@ remarklet.activate = function () {
 /**
  * @alias module:@zw/remarklet.deactivate
  * @since 1.0.0
- * @type {function}
+ * @function
  * @description Deactivates the Remarklet library, cleaning up any resources or event listeners.
  * @returns {void}
  */
 remarklet.deactivate = function () {
     state.set("active", false);
+};
+
+/**
+ * @alias module:@zw/remarklet.version
+ * @since 1.0.2
+ * @type {string}
+ * @readonly
+ * @description Get the current version of the Remarklet library.
+ */
+remarklet.version = pkg.version;
+
+/**
+ * @deprecated
+ * @alias module:@zw/remarklet.options
+ * @since 1.1.0
+ * @function
+ * @description Deprecated. An alias of remarklet.config. Will be removed in v2.0.0.
+ * @param {Object} options - The configuration options.
+ * @param {boolean} options.persist - Whether to persist the state of the page between sessions.
+ * @param {boolean} options.hide - Whether to hide certain elements.
+ * @returns {void}
+ */
+remarklet.options = remarklet.config;
+
+/**
+ * @alias module:@zw/remarklet.restore
+ * @since 1.2.0
+ * @function
+ * @description Restores the persisted changes, if any. Runs before the interactive features are initialized.
+ * @returns {void}
+ */
+remarklet.restore = function () {
+    if (state.get("loading") === true) {
+        console.warn("Loading is already in progress.");
+        return;
+    }
+    state.set("loading", true);
+    if (config.persist !== true) {
+        config.persist = true;
+    }
+    app.use(changeMap);
+    app.use(styles);
+    state.set("loading", false);
+};
+
+/**
+ * @alias module:@zw/remarklet.config
+ * @since 1.3.0
+ * @description Configures the library's features.
+ * @param {Object} options - The configuration options.
+ * @param {boolean} options.persist - Whether to persist the state of the page between sessions.
+ * @param {boolean} options.hide - Whether to hide certain elements.
+ * @returns {void}
+ */
+remarklet.config = function (options) {
+    if (options.persist === true) {
+        config.persist = true;
+    }
+    if (options.hide === true) {
+        config.hide = true;
+    }
+};
+
+/**
+ * @alias module:@zw/remarklet.unstore
+ * @since 1.3.0
+ * @function
+ * @description Deletes all Remarklet data from localStorage.
+ * @returns {void}
+ */
+remarklet.unstore = function () {
+    changeMap.unstore();
 };
 
 export default remarklet;
