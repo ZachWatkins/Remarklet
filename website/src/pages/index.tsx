b06e21a0fc15315cc5164099d013b4d14364d2bf
ZachWatkins <3458875+ZachWatkins@users.noreply.github.com>
2025-05-05 20:58:34 -0500

    Add buttons

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -1,25 +1,50 @@
 import type { ReactNode } from "react";
+import { useState } from "react";
 import clsx from "clsx";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import remarklet from "@zw/remarklet";
 
 import styles from "./index.module.css";
 
 function HomepageHeader() {
     const { siteConfig } = useDocusaurusContext();
+    const [copied, setCopied] = useState(false);
+    const [activated, setActivated] = useState(false);
     return (
         <header className={clsx("hero hero--primary", styles.heroBanner)}>
             <div className="container">
                 <h1 className="hero__title">{siteConfig.title}</h1>
                 <p className="hero__subtitle">{siteConfig.tagline}</p>
-                <div className={styles.buttons}>
+                <div className={styles.buttons + " " + styles.gap4}>
                     <Link
                         className="button button--secondary button--lg"
-                        to="/docs/intro"
+                        onClick={() => {
+                            if (activated) {
+                                remarklet.deactivate();
+                            } else {
+                                remarklet.activate();
+                            }
+                            setActivated(!activated);
+                        }}
                     >
-                        Docusaurus Tutorial - 5min ⏱️
+                        {activated ? "Deactivate" : "Activate"}
+                    </Link>
+                    <Link
+                        className="button button--secondary button--lg"
+                        onClick={() => {
+                            navigator.clipboard.writeText(
+                                "javascript:(function(){const script=document.createElement('script');script.src='https://unpkg.com/@zw/remarklet/dist/remarklet.min.js';document.head.appendChild(script);script.onload=()=>{remarklet.activate()}})();",
+                            );
+                            setCopied(true);
+                            window.setTimeout(() => {
+                                setCopied(false);
+                            }, 1500);
+                        }}
+                    >
+                        {copied ? "Copied!" : "Copy the Bookmarklet"}
                     </Link>
                 </div>
             </div>
